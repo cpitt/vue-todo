913b18f536a16a6d8ac2da0f2269480c3b7a9071
cpitt <cameron.pitt@slalom.com>
2019-04-26 15:24:42 -0700

    Updated LandingPage

--- src/components/LandingPage/LandingPage.vue
+++ src/components/LandingPage/LandingPage.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <v-btn to="/todos">
-      {{ message }}
-    </v-btn>
-  </div>
+  <v-container>
+    <v-card>
+      <v-card-title primary-title>
+        <span class="headline">Welcome</span>
+      </v-card-title>
+      <v-card-text>
+        This application was written in an effort to learn Vue and attempt to
+        apply a micro-frontend approach to Vue
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn flat to="/todos">
+          Go To Todos
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
-
-<script>
-export default {
-  data: () => ({
-    message: 'Go To Todos',
-  }),
-};
-</script>
