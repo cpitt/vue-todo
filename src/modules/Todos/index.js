329df48e87f6e0504bdc80b65cbdac26d2a4391b
Cameron Pitt <cpitt@users.noreply.github.com>
2019-05-21 11:12:54 -0700

    Fixed typo in Todos/index.js

--- src/modules/Todos/index.js
+++ src/modules/Todos/index.js
@@ -1,4 +1,4 @@
-// Each module should expoer it's main component/view as the default
+// Each module should export it's main component/view as the default
 export { default } from './Todos.vue';
 // In addition each module should have named exports of routes and store
 export { default as routes } from './routes';
